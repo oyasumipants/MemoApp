175d12d3659cda92255c4259397b88125610f583
KeigoIbaraki <keigo.ibaraki@siiibo.com>
2021-02-05 02:32:24 +0900

    Create Memo Detail Screen

---
+++ src/screens/MemoListScreens.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import AppBar from '../components/AppBar';
+import MemoList from '../components/MemoList';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoListScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <MemoList />
+      <CircleButton>+</CircleButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // bg-cream
+    backgroundColor: '#fdfdf9',
+  },
+});
