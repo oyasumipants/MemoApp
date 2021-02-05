af940fb3d717e3f58bea5a3818449f597ae3ba69
KeigoIbaraki <keigo.ibaraki@siiibo.com>
2021-02-05 23:45:39 +0900

    Create Screen

---
+++ src/screens/MemoCreateScreen.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  View, StyleSheet, TextInput, KeyboardAvoidingView,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoCreateScreen() {
+  return (
+    // キーボードの高さ分，containerの押し上げを行なっている．
+    <KeyboardAvoidingView style={styles.container} behavior="height">
+      <AppBar />
+      <View style={styles.inputContainer}>
+        <TextInput value="" multiline style={styles.input} />
+      </View>
+      <CircleButton name="check" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
