492ef11d1d7b252738da85762db90d814ac7e239
KeigoIbaraki <keigo.ibaraki@siiibo.com>
2021-02-06 15:13:05 +0900

    Create LogIn and SignUp Screen

---
+++ src/screens/SignUpScreen.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  View, Text, StyleSheet, TextInput,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import Button from '../components/Button';
+
+export default function SignUpScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <Button label="Submit" />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Already registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f4f8',
+  },
+  inner: {
+    paddingHorizontal: 19,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: '#dddddd',
+    borderWidth: 1,
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#fcc898',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+});
