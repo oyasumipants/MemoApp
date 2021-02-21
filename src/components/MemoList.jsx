72a4f9ef83d205a7bfa6c667e55f63b1a749aaac
KeigoIbaraki <keigo.ibaraki@siiibo.com>
2021-02-22 01:31:16 +0900

    Update Layout

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -44,7 +44,7 @@ export default function MemoList(props) {
         style={styles.memoListItem}
         onPress={() => { navigation.navigate('MemoDetail', { id: item.id }); }}
       >
-        <View>
+        <View style={styles.memoInner}>
           {/* numberOfLines はindex行目だけを表示させる */}
           <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
           <Text style={styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
@@ -93,6 +93,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderBottomWidth: 1,
     borderColor: 'rgba(0, 0, 0, 0.15)',
+  },
+  memoInner: {
+    flex: 1,
   },
   memoListItemTitle: {
     fontSize: 16,
